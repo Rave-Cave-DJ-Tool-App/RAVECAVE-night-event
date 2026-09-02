13697e67635d4356b5961d4c9abd4be4367288a4
RAVECAVE-tool <synfloodmusic@gmail.com>
2026-09-02 21:07:39 +0900

    Create config.js

---
+++ js /config.js
@@ -0,0 +1,7 @@
+// Google Apps Script (GAS) のデプロイURLを設定します
+const CONFIG = {
+  // 後ほどGASを作成した後に取得できるWeb App URLをここに貼り付けます
+  GAS_API_URL: "https://script.google.com/macros/s/AKfycbwmgfkg9gyW42sVdhzg9AxBkLO6KzJFNC_RQxk9HxuW0V23XckNNq6vYKKYEOX6dT_c/exec",
+  // 自動更新のチェック間隔（ミリ秒）： 30,000ms = 30秒
+  AUTO_REFRESH_INTERVAL: 30000
+};
