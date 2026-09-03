e77058b2cbe6f22c254fcb80c6eba732bec4d981
RAVECAVE-tool <synfloodmusic@gmail.com>
2026-09-03 09:58:07 +0900

    Delete js  directory

--- js /config.js
+++
@@ -1,7 +0,0 @@
-// Google Apps Script (GAS) のデプロイURLを設定します
-const CONFIG = {
-  // 後ほどGASを作成した後に取得できるWeb App URLをここに貼り付けます
-  GAS_API_URL: "https://script.google.com/macros/s/AKfycbxzfc8vTU1hRNYxFR92JYGDkh7c33ukZPUonB9AfN4sAW5FBVfXq23wjvnWoBF2fcgm/exec",
-  // 自動更新のチェック間隔（ミリ秒）： 30,000ms = 30秒
-  AUTO_REFRESH_INTERVAL: 30000
-};
